e030029a39922504118839dc60226d99cdbc7702
ПК <ekrapostin@gmail.com>
2023-01-11 17:15:26 +0300

    Методы toString, equals и hashCode реализованы

--- src/pro/sky/java/course1/homework13/Book.java
+++ src/pro/sky/java/course1/homework13/Book.java
@@ -1,5 +1,7 @@
 package pro.sky.java.course1.homework13;
 
+import java.util.Objects;
+
 public class Book {
     private String name;
     private Author author;
@@ -27,5 +29,25 @@ public class Book {
         this.pubilshDate = pubilshDate;
     }
 
+    @Override
+    public String toString() {
+        return "Книга{" +
+                "Название='" + name + '\'' +
+                ", Автор=" + author +
+                ", Год издания=" + pubilshDate +
+                '}';
+    }
 
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Book book = (Book) o;
+        return pubilshDate == book.pubilshDate && name.equals(book.name) && author.equals(book.author);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, author, pubilshDate);
+    }
 }
